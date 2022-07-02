9cd8a5375d261b7b32409287ea466a8d2946107c
shogo4131 <shougo08697118@gmail.com>
2022-07-02 16:27:40 +0900

    feat: add storybook setting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import * as nextImage from 'next/image';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import '../src/styles/globals.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  /* storybookでnext/routerを使用する設定 */
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
+
+/* storybookでnext/imageを使用する設定 */
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
