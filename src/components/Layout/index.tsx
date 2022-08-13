c20df958e29520274d00d0e01429a9297579c1d6
shogo4131 <shougo08697118@gmail.com>
2022-08-13 15:53:14 +0900

    fix: import path

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,10 @@ import { FC, ReactNode } from 'react';
 import clsx from 'clsx';
 import { useMedia } from 'use-media';
 
-import { Navigation } from '..//Navigation/index';
-import { Footer } from '../Footer/index';
-import { Header } from '../Header/index';
-import { Sidebar } from '../Sidebar/index';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Navigation } from '@/components/Navigation';
+import { Sidebar } from '@/components/Sidebar';
 
 import styles from './index.module.css';
 
