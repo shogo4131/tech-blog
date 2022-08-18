889eb2bfb08e6e22d3832cd2759ac465ecbc0ca0
shogo4131 <shougo08697118@gmail.com>
2022-08-18 21:59:55 +0900

    fix: add ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   reactStrictMode: true,
+  ignoreBuildErrors: true,
 };
 
 module.exports = withBundleAnalyzer({
