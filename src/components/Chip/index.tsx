1a7a536449e735f82bd243bad80f4b26e2645cea
shogo4131 <shougo08697118@gmail.com>
2022-08-19 19:07:15 +0900

    feat: add chip component

---
+++ src/components/Chip/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+
+import Link from 'next/link';
+
+import clsx from 'clsx';
+
+import { Tag } from '@/types/api';
+
+import styles from './index.module.css';
+
+type Props = {
+  className?: string;
+  tagClassName?: string;
+  tags: Tag[];
+};
+
+export const Chip: FC<Props> = ({ className, tagClassName, tags }) => {
+  return (
+    <div className={clsx(styles.tags, className)}>
+      {tags.map(({ id, tag }) => (
+        <Link href={`/tags/${id}`} key={id}>
+          <span className={clsx(styles.tag, tagClassName)}>
+            <a>{tag}</a>
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+};
