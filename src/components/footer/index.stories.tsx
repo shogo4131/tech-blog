510efff7ee260c0e568d953b2598c95805b06659
shogo4131 <shougo08697118@gmail.com>
2022-09-06 14:34:37 +0900

    fix: footer component

--- src/components/footer/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-
-import { Footer } from './index';
-
-export default {
-  component: Footer,
-} as ComponentMeta<typeof Footer>;
-
-export const Default: ComponentStoryObj<typeof Footer> = {};
