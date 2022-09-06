4e1174af8530a8db57574cc55d8f438a42bcf4e4
shogo4131 <shougo08697118@gmail.com>
2022-09-06 14:39:20 +0900

    fix: remove header

--- src/components/header/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-
-import { Header } from './index';
-
-export default {
-  component: Header,
-} as ComponentMeta<typeof Header>;
-
-export const Default: ComponentStoryObj<typeof Header> = {};
