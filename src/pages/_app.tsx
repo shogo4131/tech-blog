327b2a65ffb30eab8e50f0273f82cb8ef1c8de2d
shogo4131 <shougo08697118@gmail.com>
2022-10-09 16:11:23 +0900

    feat: add gtm tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css';
 import 'destyle.css';
+import { useEffect } from 'react';
+
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import TagManager from 'react-gtm-module';
+
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID || '' });
+  }, []);
+
   return (
     <>
       <Head>
