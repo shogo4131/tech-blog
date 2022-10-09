4c99c967a424b6249b543f801047218503521760
shogo4131 <60390181+shogo4131@users.noreply.github.com>
2022-10-09 16:41:38 +0900

    Revert "Release/v1.1.0"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import '../styles/globals.css';
 import 'destyle.css';
-import { useEffect } from 'react';
-
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import TagManager from 'react-gtm-module';
-
 const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID || '' });
-  }, []);
-
   return (
     <>
       <Head>
