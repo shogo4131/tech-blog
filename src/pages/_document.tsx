c77f390b38644f49a976517e666148b1503f308d
shogo4131 <60390181+shogo4131@users.noreply.github.com>
2022-10-09 18:47:35 +0900

    Revert "Revert "Release/v1.1.0""

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,32 +1,11 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Main, NextScript } from 'next/document';
 
 /* eslint-disable @next/next/next-script-for-ga */
 class Document extends NextDocument {
   render() {
     return (
       <Html lang="ja">
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer', ${process.env.NEXT_PUBLIC_GTM_ID || ''});
-          `,
-            }}
-          />
-        </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${
-                process.env.NEXT_PUBLIC_GTM_ID || ''
-              }"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
           <Main />
           <NextScript />
         </body>
