cc4c054e3e6afd732f8e7054ca63efdd2cd3b6c3
shogo4131 <shougo08697118@gmail.com>
2022-12-17 16:32:20 +0900

    fix: pagenation comoponent

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,91 +1,30 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 
-import { clsx } from 'clsx';
+import Link from 'next/link';
 
-import { range } from '../../utils/range';
+import clsx from 'clsx';
+
+import { range } from '@/utils/range';
 
 import styles from './index.module.css';
 
 export type Props = {
   className?: string;
-  activePage: number;
-  totalPageSize: number;
-  onChange: (page: number) => void;
+  totalCount: number;
 };
 
-const START_PAGE = 1;
-const DIFFERENCE = 1;
-const DISPLAY_PAGE_SIZE = 5;
-const STATIC_POSITION_NUMBER = 5;
+const PER_PAGE = 9;
 
-export const Pagination: FC<Props> = ({ className, activePage, totalPageSize, onChange }) => {
-  const min = useMemo(() => {
-    let minPage: number;
-    if (activePage < STATIC_POSITION_NUMBER) minPage = START_PAGE;
-
-    if (totalPageSize - activePage < DISPLAY_PAGE_SIZE - STATIC_POSITION_NUMBER) {
-      minPage = totalPageSize - DISPLAY_PAGE_SIZE + 1;
-    } else {
-      minPage = activePage - STATIC_POSITION_NUMBER + 1;
-    }
-
-    return Math.max(Math.min(minPage, totalPageSize), START_PAGE);
-  }, [activePage, totalPageSize]);
-
-  const max = useMemo(() => {
-    return Math.min(min + DISPLAY_PAGE_SIZE - 1, totalPageSize);
-  }, [min, totalPageSize]);
-
-  const displayLength = useMemo(() => max - min + 1, [max, min]);
-
+export const Pagenation: FC<Props> = ({ className, totalCount }) => {
   return (
     <div className={clsx(styles.root, className)}>
-      <div
-        className={clsx(styles.buttonContainer, {
-          [styles.hiddenButton]: activePage === START_PAGE,
-        })}
-      >
-        <button
-          type="button"
-          className={styles.arrowIcon}
-          disabled={activePage === START_PAGE}
-          onClick={() => onChange(START_PAGE)}
-        >
-          <img src="/images/arrow-right-double-gray.svg" alt="top" height={22} width={22} />
-        </button>
-        <button
-          type="button"
-          className={styles.arrowIcon}
-          onClick={() => onChange(Math.max(activePage - DIFFERENCE, START_PAGE))}
-        >
-          <img src="/images/arrow-right-gray.svg" alt="prev" height={20} width={20} />
-        </button>
-      </div>
-      {range(displayLength, min).map((num) => (
-        <button
-          type="button"
-          key={num}
-          className={clsx(styles.numButton, { [styles.active]: num === activePage })}
-          onClick={() => onChange(num)}
-        >
-          {num}
-        </button>
-      ))}
-      <div
-        className={clsx(styles.buttonContainer, {
-          [styles.hiddenButton]: activePage === totalPageSize,
-        })}
-      >
-        <button
-          type="button"
-          onClick={() => onChange(Math.min(activePage + DIFFERENCE, totalPageSize))}
-        >
-          <img src="/images/arrow-right-gray.svg" alt="next" height={20} width={20} />
-        </button>
-        <button type="button" onClick={() => onChange(totalPageSize)}>
-          <img src="/images/arrow-right-double-gray.svg" alt="last" height={20} width={20} />
-        </button>
-      </div>
+      <ul>
+        {range(1, Math.ceil(totalCount / PER_PAGE)).map((number) => (
+          <li key={number}>
+            <Link href={`/page/${number}`}>{number}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
