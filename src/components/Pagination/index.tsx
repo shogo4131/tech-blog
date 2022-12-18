8c12bd671620d25a4da7dcf2a54e599fc322e823
shogo4131 <shougo08697118@gmail.com>
2022-12-18 23:38:03 +0900

    fix: change click area

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -11,17 +11,20 @@ import styles from './index.module.css';
 export type Props = {
   className?: string;
   totalCount: number;
+  pageUrl: string;
 };
 
 const PER_PAGE = 9;
 
-export const Pagenation: FC<Props> = ({ className, totalCount }) => {
+export const Pagenation: FC<Props> = ({ className, totalCount, pageUrl }) => {
   return (
     <div className={clsx(styles.root, className)}>
       <ul className={styles.pageNum}>
         {range(1, Math.ceil(totalCount / PER_PAGE)).map((number) => (
-          <li key={number} className={styles.num}>
-            <Link href={`/page/${number}`}>{number}</Link>
+          <li key={number}>
+            <Link href={`${pageUrl}/${number}`} className={styles.num}>
+              {number}
+            </Link>
           </li>
         ))}
       </ul>
