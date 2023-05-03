5a2cc7729f1edca9a99497bdf55457e1cb1624c9
shogo4131 <shougo08697118@gmail.com>
2023-05-03 20:44:26 +0900

    feat: storybookにvrtの記述を追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
+import { withScreenshot } from 'storycap';
+
 import '../src/styles/globals.css';
 import 'destyle.css';
+
+export const decorators = [withScreenshot];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
