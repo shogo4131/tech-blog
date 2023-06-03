25d4ebd90420769ee560555f6fd2ebead0f3fd0e
shogo.fukami <shogo.fukami@route06.co.jp>
2023-06-03 11:13:25 +0900

    fix: yarn dev時にpathを生成しないようにする

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,42 +1,62 @@
 export const pagesPath = {
-  "$404": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
+  $404: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash }),
   },
-  "blog": {
+  blog: {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
-    })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/blog/[id]' as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
   },
-  "category": {
+  category: {
     _slug: (slug: string | number) => ({
-      "page": {
+      page: {
         _id: (id: string | number) => ({
-          $url: (url?: { hash?: string }) => ({ pathname: '/category/[slug]/page/[id]' as const, query: { slug, id }, hash: url?.hash })
-        })
+          $url: (url?: { hash?: string }) => ({
+            pathname: '/category/[slug]/page/[id]' as const,
+            query: { slug, id },
+            hash: url?.hash,
+          }),
+        }),
       },
-      $url: (url?: { hash?: string }) => ({ pathname: '/category/[slug]' as const, query: { slug }, hash: url?.hash })
-    })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/category/[slug]' as const,
+        query: { slug },
+        hash: url?.hash,
+      }),
+    }),
   },
-  "page": {
+  page: {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/page/[id]' as const, query: { id }, hash: url?.hash })
-    })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/page/[id]' as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
   },
-  "privacy": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/privacy' as const, hash: url?.hash })
+  privacy: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/privacy' as const, hash: url?.hash }),
   },
-  "profile": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/profile' as const, hash: url?.hash })
+  profile: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/profile' as const, hash: url?.hash }),
   },
-  "sitemap": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/sitemap' as const, hash: url?.hash })
+  sitemap: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sitemap' as const, hash: url?.hash }),
   },
-  "tags": {
+  tags: {
     _slug: (slug: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/tags/[slug]' as const, query: { slug }, hash: url?.hash })
-    })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/tags/[slug]' as const,
+        query: { slug },
+        hash: url?.hash,
+      }),
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
+};
 
-export type PagesPath = typeof pagesPath
+export type PagesPath = typeof pagesPath;
